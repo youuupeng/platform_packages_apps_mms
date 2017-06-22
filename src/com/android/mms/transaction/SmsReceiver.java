af17fb8a84f083b7cd8f2326a2ff40b1df6ba040
youpeng <1245300590@qq.com>
2017-06-23 00:43:26 +0800

    添加注释
    SmsReceiver的作用主要是启动SmsReceiverService类处理接收到的短信事件通知
    + onReceiveWithPrivilege的方法的作用？
    + onReceiveWithPrivilege(context, intent, false);这边为什么传入的参数是false？
    + beginStartingService的时候启动唤醒锁的作用？
    + synchronized (mStartingServiceSync)同步锁的作用是防止同时来短信启动service？
    + intent.putExtra("result", getResultCode())传递的数据包含哪些
    + beginStartingService()方法为静态的？其他哪些地方有调用？

--- src/com/android/mms/transaction/SmsReceiver.java
+++ src/com/android/mms/transaction/SmsReceiver.java
@@ -26,9 +26,11 @@ import android.provider.Telephony.Sms.Intents;
 
 /**
  * Handle incoming SMSes.  Just dispatches the work off to a Service.
+ * 这个广播通过监听frameworks收到短信时发出的通知实现监听接收短信。并且在Service中处理即将到来的短信
  */
 public class SmsReceiver extends BroadcastReceiver {
     static final Object mStartingServiceSync = new Object();
+    //唤醒锁
     static PowerManager.WakeLock mStartingService;
     private static SmsReceiver sInstance;
 
@@ -49,6 +51,8 @@ public class SmsReceiver extends BroadcastReceiver {
         // no-permissions receiver class.  If we get an SMS_RECEIVED message that way, it
         // means someone has tried to spoof the message by delivering it outside the normal
         // permission-checked route, so we just ignore it.
+        //如果privileged为false，意味着这个intent会被传递给无权限的接收类。如果用这种方式获取SMS_RECEICED短信，意味着可能
+        //原始的短信会被经过处理，所以要忽视它，这边直接return
         if (!privileged && intent.getAction().equals(Intents.SMS_DELIVER_ACTION)) {
             return;
         }
@@ -64,6 +68,7 @@ public class SmsReceiver extends BroadcastReceiver {
     // factor them out or, even better, improve the API for starting
     // services under wake locks.
 
+    //开启service来处理当前的事件通知，在开启之前先启动唤醒锁
     /**
      * Start the service to process the current event notifications, acquiring
      * the wake lock before returning to ensure that the service will run.
@@ -82,6 +87,7 @@ public class SmsReceiver extends BroadcastReceiver {
         }
     }
 
+    //用于service处理完通知后调用该方法，释放唤醒锁
     /**
      * Called back by the service when it has finished processing notifications,
      * releasing the wake lock if the service is now stopping.
